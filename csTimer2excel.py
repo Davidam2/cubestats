e289193653bc3b8d1322b2a6a6b7b008f70d2c53
Davidam2 <david.alvar9@gmail.com>
2025-03-16 10:54:56 +0100

    Updated to save session info separately

--- csTimer2excel.py
+++ csTimer2excel.py
@@ -1,15 +1,15 @@
 """
-Creates a .xlsx file from a csTimer .txt file.
+Creates a .csv file from a csTimer .txt file.
 
 Usage:
     python csTimer2excel.py <input_file> <output_file>
 
 Arguments:
     input_file: The .txt file to be converted.
-    output_file: The .xlsx file to be created.
+    output_file: The .csv file to be created.
 
 Example:
-    python csTimer2excel.py input.txt output.xlsx
+    python csTimer2excel.py input.txt output.csv
 
 Dependencies:
     pandas
@@ -20,78 +20,204 @@ Author:
 
     GitHub: @Davidam2
     
-    Date: March 14th, 2025
+    Date start: March 14th, 2025
+
+    Last update: March 15th, 2025
 """
 
+import numpy as np
 import argparse
 import json
 import pandas as pd
 from datetime import datetime
 
-
 class csTimer2excel:
     def __init__(self, input_file, output_file = None):
-        self.input_file = input_file
+        self.input_file = open(input_file, 'r')
         self.output_file = output_file
 
-    def read(self):
-        """
-        Reads the .txt file.
-        """
-        self.file = open(self.input_file, 'r')
-        self.data = json.load(self.file)
-        self.data_keys = self.data.keys()
+        self.data = json.load(self.input_file)
         self.properties = self.data['properties']
-        self.prop_keys = self.properties.keys()
+        self.sessionData = json.loads(self.properties['sessionData'])
+
+        self.sessions_df = pd.DataFrame(columns = ['Name', 'Num solves',
+                                                   'Date start', 'Date end',
+                                                   'Average', 'Best time',
+                                                   'Best avg5', 'Best avg12',
+                                                   'Best avg100', 'Best avg1000',
+                                                   'Best avg5000', 'Best avg10000'])
+        self.sessions_df = self.sessions_df.astype({'Name': 'object',
+                                                   'Num solves': 'int',
+                                                   'Date start': 'object',
+                                                   'Date end': 'object',
+                                                   'Average': 'float',
+                                                   'Best time': 'float',
+                                                   'Best avg5': 'float',
+                                                   'Best avg12': 'float',
+                                                   'Best avg100': 'float',
+                                                   'Best avg1000': 'float',
+                                                   'Best avg5000': 'float',
+                                                   'Best avg10000': 'float'})
+        
 
     def get_dates(self, sessionIndex):
         """
         Gets the start and end dates of the session.
         """
-        sessionData = self.properties['sessionData']
-        sessionData = json.loads(sessionData)
-        session_keys = sessionData.keys()
-        for session in session_keys:
-            print('Session:', session)
-            session_start = sessionData[session]['date'][0]
-            session_end = sessionData[session]['date'][1]
-            date_start = datetime.fromtimestamp(session_start, tz=None).strftime('%Y-%m-%d %H:%M:%S')
-            date_end = datetime.fromtimestamp(session_end, tz=None).strftime('%Y-%m-%d %H:%M:%S')
+        session_start = self.sessionData[str(sessionIndex)]['date'][0]
+        session_end = self.sessionData[str(sessionIndex)]['date'][1]
+        date_start = datetime.fromtimestamp(session_start, tz=None).strftime(
+            '%Y-%m-%d %H:%M:%S')
+        date_end = datetime.fromtimestamp(session_end, tz=None).strftime(
+            '%Y-%m-%d %H:%M:%S')
         return date_start, date_end
+
+
+    def analyze_time(self, scramble):
+        """
+        Analyzes the time of the scramble.
+        """
+        penalty = scramble[0][0]
+        time = scramble[0][1]/1000
+        date = datetime.fromtimestamp(scramble[-1], tz=None).strftime(
+            '%Y-%m-%d %H:%M:%S')
+        mix = scramble[1]
+
+        return penalty, time, date, mix
+
+
+    def get_stat(self, sessionIdx):
+        """
+        Analyzes the session.
+        """
+        session_stat = self.properties['sessionData'][str(sessionIdx)]['stat']
+        num_soves = session_stat[0]
+        session_avg = session_stat[2]
+
+
+    def calculate_avg(self, times):
+        """
+        Calculates the average from a list of solves.
+        
+        Removes the 5% best and worst solves and calculates the average.
+        """
+        sorted_times = sorted(times)
+        remove = int(np.ceil(len(sorted_times) * 0.05))
+        sorted_times = sorted_times[remove:-remove]
+        avg = np.round(sum(sorted_times) / len(sorted_times), 3)
+        return avg
+
+
+    def save_session(self, sessionIdx):
+        """
+        Saves the session data.
+        """
+        start, end = self.get_dates(sessionIdx)
+        session_name = self.sessionData[str(sessionIdx)]['name']
+        num_solves = self.sessionData[str(sessionIdx)]['stat'][0]
+        avg = np.round(self.sessionData[str(sessionIdx)]['stat'][2]/1000, 3)
+
+        self.sessions_df = pd.concat([self.sessions_df, pd.DataFrame(
+            {'Name': [session_name], 'Num solves': [num_solves],
+             'Date start': [start], 'Date end': [end], 'Average': [avg],
+             'Best time': '', 'Best avg5': '', 'Best avg12': '',
+             'Best avg100': '', 'Best avg1000': '', 'Best avg5000': '',
+             'Best avg10000': ''})], ignore_index=True)
+
 
     def convert(self):
         """
-        Converts the .txt file to a .xlsx file.
+        Converts the .txt file to a .csv file.
         """
-        self.read()
-        print('Converting', self.input_file)
-        print('Data keys:', self.data_keys)
-        print('Properties:', self.prop_keys)
+        # Create a DataFrame
+        df = pd.DataFrame(columns = ['Session', 'Num', 'Date', 'Time', 'Penalty',
+                                     'Mix', 'avg5', 'avg12', 'avg100', 'avg1000',
+                                     'avg5000', 'avg10000'])
+        df = df.astype({'Session': 'object', 'Num': 'int', 'Date': 'object',
+                        'Time': 'float', 'Penalty': 'object', 'Mix': 'object',
+                        'avg5': 'float', 'avg12': 'float', 'avg100': 'float',
+                        'avg1000': 'float', 'avg5000': 'float', 'avg10000': 'float'
+        })
+        
         num_sessions = self.properties['sessionN'] # Number of sessions
 
         for i in range (1, num_sessions + 1):
-            print('Session:', i)
+            self.save_session(i)
             date_start, date_end = self.get_dates(i)
-            print('Session:', i)
-            print('Start date:', date_start)
-            print('End date:', date_end)
-            
-        self.file.close()
+            session = self.data['session' + str(i)]
+            name_session = self.sessionData[str(i)]['name']
+
+            # Dictionary with averages and best averages
+            dict_avgs = {'avg5': [], 'avg12': [], 'avg100': [], 'avg1000': [],
+                         'avg5000': [], 'avg10000': []}
+            current_avgs = {'avg5': '', 'avg12': '', 'avg100': '', 'avg1000': '',
+                           'avg5000': '', 'avg10000': ''}
+            best_avg = {'avg1': 100, 'avg5': '', 'avg12': '', 'avg100': '',
+                        'avg1000': '', 'avg5000': '', 'avg10000': ''}
+
+            for j, scramble in enumerate(session):
+                penalty, time, date, mix = self.analyze_time(scramble)
+
+                # Calculate the best averages
+                if time < best_avg['avg1']:
+                    best_avg['avg1'] = time
+                for avg in ['avg5', 'avg12', 'avg100', 'avg1000', 'avg5000',
+                            'avg10000']:
+                    if len (dict_avgs[avg]) >= int(avg[3:]):
+                        dict_avgs[avg].pop(0)
+                    dict_avgs[avg].append(time)
+                    if len(dict_avgs[avg]) == int(avg[3:]):
+                        avg_time = self.calculate_avg(dict_avgs[avg])
+                        current_avgs[avg] = avg_time
+                        if best_avg[avg] == '':
+                            best_avg[avg] = avg_time
+                        elif avg_time < best_avg[avg]:
+                            best_avg[avg] = avg_time
+
+                df = pd.concat([df, pd.DataFrame(
+                    {'Session': [name_session], 'Num':j+1, 'Date': [date], 
+                     'Time': [time], 'Penalty': [penalty], 'Mix': [mix],
+                     'avg5': [current_avgs['avg5']],
+                     'avg12': [current_avgs['avg12']],
+                     'avg100': [current_avgs['avg100']],
+                     'avg1000': [current_avgs['avg1000']],
+                     'avg5000': [current_avgs['avg5000']],
+                     'avg10000': [current_avgs['avg10000']]} )],
+                     ignore_index=True)
+
+            # Save the best averages
+            self.sessions_df.at[i-1, 'Best time'] = best_avg['avg1']
+            self.sessions_df.at[i-1, 'Best avg5'] = best_avg['avg5']
+            self.sessions_df.at[i-1, 'Best avg12'] = best_avg['avg12']
+            self.sessions_df.at[i-1, 'Best avg100'] = best_avg['avg100']
+            self.sessions_df.at[i-1, 'Best avg1000'] = best_avg['avg1000']
+            self.sessions_df.at[i-1, 'Best avg5000'] = best_avg['avg5000']
+            self.sessions_df.at[i-1, 'Best avg10000'] = best_avg['avg10000']
+       
+        # Save the DataFrames to .csv files
+        df.to_csv(self.output_file, index = False)
+        self.sessions_df.to_csv('sessions.csv', index = False)
+
+        self.input_file.close()
+
 
     def save(self):
         pass
+    
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Converts a csTimer .txt file to a .xlsx file.')
-    parser.add_argument('--input_file', '-i', type=str, help='The .txt file to be converted.')
-    # parser.add_argument('--output_file', '-o', type=str, help='The .xlsx file to be created.')
+    parser = argparse.ArgumentParser(
+        description='Converts a csTimer .txt file to a .xlsx file.')
+    parser.add_argument('--input_file', '-i', type=str,
+                        help='The .txt file to be converted.')
+    parser.add_argument('--output_file', '-o', type=str,
+                        help='The .xlsx file to be created.')
     args = parser.parse_args()
 
     input_file = args.input_file
-    # output_file = args.output_file
+    output_file = args.output_file
 
-    # converter = csTimer2excel(input_file, output_file)
-    converter = csTimer2excel(input_file)
+    converter = csTimer2excel(input_file, output_file)
     converter.convert()
     converter.save()
 
